d6dc47e78a41679c72f84d80b32bffd8d07e6716
iamgauraav <iamgauraav@gmail.com>
2022-03-28 23:43:53 +0530

    Added Welcome message to master branch

---
+++ DSStacksandQueues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSStacksandQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to  Data Structure Stack And Queues on master branch");
+        } 
+    }
+}
